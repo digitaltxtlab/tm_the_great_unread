2a612938e2ecac3bafd4d040ee41613be8ee23ce
digitaltxtlab <kln@cas.au.dk>
2016-07-28 12:35:21 +0200

    Update util_fun.R

--- util_fun.R
+++ util_fun.R
@@ -5,3 +5,7 @@ resave <- function(..., list = character(), file) {
   for (var in var.names) assign(var, get(var, envir = parent.frame()))
   save(list = unique(c(previous, var.names)), file = file)
 }
+# slice text in n bins
+slice_text <- function(text,bin){
+  sliced.text.l <- split(text, cut(1:length(text),bin))
+}
